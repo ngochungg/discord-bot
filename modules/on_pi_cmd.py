537d03d7b7607d4f9906c373050f2aaca3625979
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2025-05-09 14:26:01 -0700

    fix !update

--- modules/on_pi_cmd.py
+++ modules/on_pi_cmd.py
@@ -24,16 +24,16 @@ project_dir = __File__.parent
 async def status(ctx):
     if ctx.author.id not in ALLOWED_USER_IDS:
         await ctx.channel.send(f"⛔️You are not allowed to use this command.")
-    await ctx.channel.send(f"📦 Checking computer...")
+    await ctx.channel.send(f"🚀 Checking status...")
     cpu = psutil.cpu_percent(interval=1)
     mem = psutil.virtual_memory()
     disk = psutil.disk_usage('/')
     uptime = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
     text = (
-        f"**💻CPU:** {cpu}%\n"
+        f"\n```bash\n**💻CPU:** {cpu}%\n"
         f"**🧠RAM:** {mem.percent}% (usable {mem.available // (1024*1024)} MB)\n"
         f"**💾Disk:** {disk.percent}% ({disk.used // (1024*1024*1024)}GB/{disk.total // (1024*1024*1024)}GB)\n"
-        f"**⏱️Uptime:** {str(uptime).split('.')[0]}"
+        f"**⏱️Uptime:** {str(uptime).split('.')[0]}\n```"
     )
     await ctx.channel.send(f"{text}")
 
@@ -41,7 +41,7 @@ async def status(ctx):
 async def docker_ps(ctx):
     if ctx.author.id not in ALLOWED_USER_IDS:
         await ctx.channel.send(f"⛔️You are not allowed to use this command.")
-
+    await ctx.channel.send(f"🐳 Checking container...")
     try:
         output = subprocess.run(['docker', 'ps'], capture_output=True, text=True, check=True)
         msg = output.stdout
@@ -51,55 +51,22 @@ async def docker_ps(ctx):
     except subprocess.CalledProcessError as e:
         await ctx.channel.send(f"Error while run docker ps: {e}")
 
-# --- !reboot: Reboot Pi (only bot owner) ---
-async def reboot(ctx):
-    if ctx.author.id not in ALLOWED_USER_IDS:
-        await ctx.channel.send(f"⛔️You are not allowed to use this command.")
-
-    await ctx.channel.send("🔄 Reboot system...")
-    subprocess.Popen(['sudo reboot'])
-
-# --- !shutdown: Shutdown Pi (only bot owner) ---
-async def shutdown(ctx):
-    if ctx.author.id not in ALLOWED_USER_IDS:
-        await ctx.channel.send(f"⛔️You are not allowed to use this command.")
-
-    await ctx.channel.send("⏻ Shutdown system...")
-    subprocess.Popen(['sudo shutdown', '-h', 'now'])
-
 # --- !update: Git pull and restart bot ---
 async def update(ctx):
+    if ctx.author.id not in ALLOWED_USER_IDS:
+        await ctx.channel.send(f"⛔️You are not allowed to use this command.")
+
     await ctx.channel.send("📦 Pulling latest code & rebuilding bot...")
-
     try:
-        response = requests.post("http://120.0.0.1:20000/update")
+        response = requests.post("http://update-manager:20000/update")
+        data = response.json()
 
-        if response.status_code == 200:
-            await ctx.channel.send(f"✅ Update completed:\n```bash\n\n```")
-        else:
-            await ctx.channel.send(f"❌ Error: {response.text}")
-
-    except subprocess.CalledProcessError as e:
-        error_output = e.stderr or e.stdout or "Unknown error"
-        snippet = error_output[:1900] + ("… (truncated)" if len(error_output) > 1900 else "")
-        await ctx.channel.send(
-            f"❌ Update failed (exit code {e.returncode}):\n```bash\n{snippet}\n```"
-        )
-
-async def test_update(ctx):
-    await ctx.channel.send("📦 Pulling latest code & rebuilding bot...")
-
-    try:
-        proc = subprocess.run(
-            ["docker", "exec", "update-manager", "bash", "/app/update-manager/update_bot.sh"],
-            capture_output=True,
-            text=True,
-            check=True
-        )
-
-        output = proc.stdout.strip() or "✅ Update completed successfully."
+        output = data.get("output", "No output")
         snippet = output[:1900] + ("… (truncated)" if len(output) > 1900 else "")
-        await ctx.channel.send(f"✅ Update completed:\n```bash\n{snippet}\n```")
+        if response.status_code == 200:
+            await ctx.channel.send(f"\n```bash\n{snippet}\n```")
+        else:
+            await ctx.channel.send(f"❌ Error: {snippet}")
 
     except subprocess.CalledProcessError as e:
         error_output = e.stderr or e.stdout or "Unknown error"
--- update-manager/main.py
+++ update-manager/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import subprocess
 
 app = Flask(__name__)
@@ -6,10 +6,17 @@ app = Flask(__name__)
 @app.route("/update", methods=["POST"])
 def update():
     try:
-        subprocess.run(["bash", "./update_bot.sh"], check=True)
-        return "Update triggered successfully", 200
+        result = subprocess.Popen(["bash", "./update_bot.sh"])
+        # return "Update triggered successfully", 200
+        return jsonify({
+            "success": True,
+            "output": result or "✅ Update bot completed."
+        }), 200
     except subprocess.CalledProcessError as e:
-        return f"Error: {e}", 500
+        return jsonify({
+            "success": False,
+            "output": e.stderr or e.stdout or str(e)
+        }), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=20000)
