817e1cc91f90d0398314eee7bbc55e44d2dea088
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2025-05-30 00:34:35 -0700

    update update_bot.sh

--- update-manager/update_bot.sh
+++ update-manager/update_bot.sh
@@ -5,14 +5,12 @@ set -e
 # Pull code mới từ GitHub
 echo "📂 Pulling latest code..."
 cd /app
-if ! git_output=$(git pull origin main 2>&1); then
-  echo "❌ Git pull failed"
-  echo "$git_output"
-  exit 1
-fi
+git_output=$(git pull origin main)
 
 if [[ "$git_output" != "Already up to date." ]]; then
     echo "♻️ Changes detected! Restarting container..."
+    echo "$git_output"
+    sleep 1
     docker restart the-herta || { echo "❌ Docker restart failed"; exit 1; }
 else
     echo "✅ No changes, no need to restart."
