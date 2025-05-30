1174f064aea5c0a444257c28de2907025b38a1aa
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2025-05-30 12:03:58 -0700

    update command

--- update-manager/update_bot.sh
+++ update-manager/update_bot.sh
@@ -10,14 +10,12 @@ git_output=$(git pull origin main)
 if echo "$git_output" | grep -q "Already up to date."; then
     echo "✅ No changes, no need to restart."
     exit 0
-
 else
     echo "♻️ Changes detected! Restarting container..."
     echo "$git_output"
-
     sleep 10
-
-    docker restart the-herta || { echo "❌ Docker restart failed"; exit 1; }
 fi
+
+# docker restart the-herta || { echo "❌ Docker restart failed"; exit 1; }
 
 echo "✅ Bot updated and restarted!"
