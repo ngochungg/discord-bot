324e39a4d94056108c27a0612b83b5744da38ef5
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2026-03-06 01:00:26 -0800

    update monitor

--- cogs/monitor.py
+++ cogs/monitor.py
@@ -14,7 +14,7 @@ class Monitor(commands.Cog):
         # Lấy thông số hệ thống
         cpu_usage = psutil.cpu_percent(interval=1)
         ram = psutil.virtual_memory()
-        disk = psutil.disk_usage('/')
+        # disk = psutil.disk_usage('/')
         uname = platform.uname()
         
         # Tạo giao diện Embed đẹp mắt
@@ -27,16 +27,13 @@ class Monitor(commands.Cog):
         embed.add_field(name="🌐 OS", value=f"{uname.system} {uname.release}", inline=False)
         embed.add_field(name="🔥 CPU Usage", value=f"{cpu_usage}%", inline=True)
         embed.add_field(name="🧠 RAM", value=f"{ram.percent}% ({ram.used//1048576}MB / {ram.total//1048576}MB)", inline=True)
-        disk_text = ""
-        partitions = psutil.disk_partitions()
-        for partition in partitions:
-
-            if 'rw' in partition.opts and not partition.mountpoint.startswith('/private'):
-
-                usage = psutil.disk_usage(partition.mountpoint)
-                disk_text += f"{partition.device}: {usage.percent}% ({usage.used//1048576}MB / {usage.total//1048576}MB)\n"
-                
-        embed.add_field(name="💾 Disk", value=disk_text or "No drive found", inline=False)
+        
+        for part in psutil.disk_partitions():
+            if part.mountpoint == '/home' or part.mountpoint == '/srv' or part.mountpoint == '/hdd/storage':
+                disk = psutil.disk_usage(part.mountpoint)
+                embed.add_field(name="💾 Disk", value=f"{disk.percent}%", inline=True)
+                break
+        
         
         embed.set_footer(text=f"Requested by {interaction.user.name}")
         
